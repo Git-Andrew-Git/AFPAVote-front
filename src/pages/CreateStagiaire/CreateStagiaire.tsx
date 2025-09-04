26ac937862e8201b2f6c15b012b9841e3e37d71a
Git-Andrew-Git <177481666+Git-Andrew-Git@users.noreply.github.com>
2025-09-04 13:03:15 +0200

    stagiaire logic

--- src/pages/CreateStagiaire/CreateStagiaire.tsx
+++ src/pages/CreateStagiaire/CreateStagiaire.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router";
 
 
@@ -5,8 +6,36 @@ const CreateStagiaire = () => {
 
     const navigate = useNavigate();
 
+    const [nom, serNom] = useState<string>("");
+    const [prenom, setPrenom] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const stagiaire = {nom, prenom, email}
+        try {
+            const res = await fetch("http://localhost:8080/api/stagiaires", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(stagiaire),
+            });
+
+            if (!res.ok) {
+                throw new Error("Failed to create stagiaire");
+                
+            }
+
+            const data = await res.json();
+            console.log("Created:", data)
+            
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
-      <>
+      <form onSubmit={handleSubmit}>
         <h1>Creation d'un Stagiaire</h1>
         <label htmlFor="nom">Nom</label>
         <input type="text" placeholder="Nom" id="nom" />
@@ -14,8 +43,10 @@ const CreateStagiaire = () => {
         <input type="text" placeholder="Prènom" id="prenom" />
         <label htmlFor="email">Email</label>
         <input type="email" name="email" id="email" placeholder="Email" />
+        <button>Valider</button>
+        <button>Annuler</button>
         <button onClick={()=>navigate("/")}>Revenir sur la page Home</button>
-      </>
+      </form>
     );
 }
 
